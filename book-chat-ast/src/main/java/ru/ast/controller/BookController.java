1c31fc954beea65e73e58b469115e4302e8f8c32
Alexey Skripnichenko <alex-himki@mail.ru>
2026-08-07 16:48:39 +0300

    немного свагера

--- book-chat-ast/src/main/java/ru/ast/controller/BookController.java
+++ book-chat-ast/src/main/java/ru/ast/controller/BookController.java
@@ -1,5 +1,12 @@
 package ru.ast.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,25 +27,70 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/books" )
 @AllArgsConstructor
+@Tag(name = "Book", description = "API для управления книгами в системе" )
 public class BookController {
 
     private final BookService bookService;
     private final String DATA_DIR = "src/main/resources/data/";
 
+
+    @Operation(
+            summary = "Добавить новую книгу",
+            description = "Сохраняет книгу из указанного локального пути и разбивает на фрагменты. Тяжелая операция "
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Книга успешно создана",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BookResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Неверные данные запроса",
+                    content = @Content
+            ),
+    })
     @PostMapping
     public ResponseEntity<BookResponseDto> addBook(@RequestBody BookRequestDto bookDto) {
         BookResponseDto response = bookService.saveBook(bookDto);
         return ResponseEntity.ofNullable(response);
     }
 
-    // Новый эндпоинт для загрузки файла через форму
-    @PreAuthorize("hasAnyRole('ADMIN')")
+    @Operation(
+            summary = "Добавить новую книгу",
+            description = "Сохраняет книгу из выбранного файла и разбивает на фрагменты. Тяжелая операция "
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Книга успешно создана",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BookResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Неверные данные запроса",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Доступ запрещен (требуются права администратора)",
+                    content = @Content
+            )
+    })
+    @PreAuthorize("hasAnyRole('ADMIN')" )
+    @SecurityRequirement(name = "basicAuth" )
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<BookResponseDto> uploadBook(
-            @RequestParam("file") MultipartFile file,
-            @RequestParam("title") String title) {
+            @RequestParam("file" ) MultipartFile file,
+            @RequestParam("title" ) String title) {
 
         try {
             // Сохраняем файл на сервере
@@ -62,7 +114,7 @@ public class BookController {
         }
     }
 
-    @GetMapping("/{bookId}")
+    @GetMapping("/{bookId}" )
     public ResponseEntity<BookResponseDto> getBook(@PathVariable UUID bookId) {
         BookResponseDto response = bookService.getBook(bookId);
         return ResponseEntity.ofNullable(response);
@@ -74,15 +126,15 @@ public class BookController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/{bookId}/chunks")
+    @GetMapping("/{bookId}/chunks" )
     public ResponseEntity<ChunksResponseDto> getChunks(@PathVariable UUID bookId) {
         ChunksResponseDto allChunks = bookService.getAllChunks(bookId);
         return ResponseEntity.ok(allChunks);
     }
 
 
-    @PreAuthorize("hasAnyRole('ADMIN')")
-    @DeleteMapping("/{bookId}")
+    @PreAuthorize("hasAnyRole('ADMIN')" )
+    @DeleteMapping("/{bookId}" )
     public ResponseEntity<String> deleteBook(@PathVariable UUID bookId) {
         boolean deleted = bookService.deleteBook(bookId);
         if (deleted) {
